5b4e9dc9b84696aa27fe79ffb96c486b05610a28
Abinesh-Srinivasan <abineshsrinivasan1301@gmail.com>
2025-02-27 11:27:47 +0530

    New Gemini 2.0 Flash API key updated

--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -24,7 +24,7 @@ const Mainpage = () => {
   ];
   const handleInput = (suggestion) => setInput(suggestion);
   const handleQuestionFromNavbar = (qnFrNav) => setQuestionFromNavbar(qnFrNav);
-  const apiUrl = import.meta.env.VITE_ApiUrl;
+  const apiUrl = import.meta.env.VITE_ApiUrl2;
 
   useEffect(() => {
     const ApiResponse = async () => {
